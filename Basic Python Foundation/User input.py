5ae3d6934bd8d560ef7dbd2d103941d25933ce79
Ubaswe <127177765+ubaswe@users.noreply.github.com>
2023-12-26 02:30:01 +0600

    Update User input.py

--- Basic Python Foundation/User input.py
+++ Basic Python Foundation/User input.py
@@ -1,19 +1,9 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[4]:
-
-
 #string by user input
 x=input("Enter an word having 5 letter :")
 print(x)
 print(x[1])
 print(x[1-3])
 print(x[-3-1])
-
-
-# In[1]:
-
 
 #Arthmatic Operation by user input
 x=int(input("Enter x value :"))
